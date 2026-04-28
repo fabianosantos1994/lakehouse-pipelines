e2277bd8d8fa8fa9fdee48a860c5ed0526f2ba26
Fabiano Santos <fabianosantos1994@gmail.com>
2026-04-28 00:59:47 -0300

    Add logging standard for Spark jobs

--- src/common/job.py
+++ src/common/job.py
@@ -2,13 +2,26 @@ from collections.abc import Callable
 
 from pyspark.sql import SparkSession
 
+from common.logger import get_logger
 from common.spark import build_spark_session
 
 
 def run_job(app_name: str, job_fn: Callable[[SparkSession], None]) -> None:
-    spark = build_spark_session(app_name)
+    spark = None
+    logger = get_logger(app_name)
+
+    logger.info("Job started")
 
     try:
+        spark = build_spark_session(app_name)
         job_fn(spark)
+    except Exception:
+        logger.exception("Job failed")
+        raise
+    else:
+        logger.info("Job succeeded")
     finally:
-        spark.stop()
+        if spark is not None:
+            logger.info("Stopping Spark session")
+            spark.stop()
+            logger.info("Spark session stopped")
